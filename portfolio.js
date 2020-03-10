5cb3b913cb65873d8367ac123e3882409f900fed
precompiled <precompiled@users.noreply.github.com>
2020-03-10 22:31:41 +0100

    Upgrade using prompt and alert
    some description lalalalalaalallalalalallaal

--- portfolio.js
+++ portfolio.js
@@ -3,35 +3,43 @@ function pkm (users_choice)
 	var values = ["rock","paper","scissors"];
 	var computers_choice = values[Math.floor(Math.random() * 3)];
 	
-	console.log("You chose " + users_choice + ", while computer chose " + computers_choice);
+	alert("You chose " + users_choice + ", while computer chose " + computers_choice);
 
     if(users_choice==computers_choice)
 	{ 
-      console.log("It's tie");
+      alert("It's tie");
 	}
 	else if(users_choice=="rock" && computers_choice=="paper")
 	{
-		console.log("Computer won")
+		alert("Computer won")
 	}
 	else if(users_choice=="rock" && computers_choice=="scissors")
 	{
-		console.log("You won")
+		alert("You won")
 	}
 	else if(users_choice=="paper" && computers_choice=="rock")
 	{
-		console.log("You won")
+		alert("You won")
 	}
 	else if(users_choice=="paper" && computers_choice=="scissors")
 	{
-		console.log("Computer won")
+		alert("Computer won")
 	}
 	else if(users_choice=="scissors" && computers_choice=="rock")
 	{
-		console.log("Computer won")
+		alert("Computer won")
 	}
 	else if(users_choice=="scissors" && computers_choice=="paper")
 	{
-		console.log("You won")
+		alert("You won")
+	} else {
+		alert("Invalid input.")
 	}
 
+}
+
+function pkm2() {
+	var choice = prompt('Rock, paper, scissors?');
+
+	pkm(choice);
 }
