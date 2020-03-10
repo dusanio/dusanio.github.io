89ff681b4c04ab0db859f659a3534d73a1abf013
precompiled <precompiled@users.noreply.github.com>
2020-03-10 22:24:40 +0100

    Minor corrections to make it work
    Minor description

--- portfolio.js
+++ portfolio.js
@@ -1,34 +1,35 @@
 function pkm (users_choice)
 {
 	var values = ["rock","paper","scissors"];
-	var computers_choice = values [Math.floor(Math.random() * 3)];
+	var computers_choice = values[Math.floor(Math.random() * 3)];
 	
+	console.log("You chose " + users_choice + ", while computer chose " + computers_choice);
 
-    if(users_choice=computers_choice)
+    if(users_choice==computers_choice)
 	{ 
       console.log("It's tie");
 	}
-	else if(users_choice="rock" and computers_choice="paper")
+	else if(users_choice=="rock" && computers_choice=="paper")
 	{
 		console.log("Computer won")
 	}
-	else(users_choice="rock" and computers_choice="scissors")
+	else if(users_choice=="rock" && computers_choice=="scissors")
 	{
 		console.log("You won")
 	}
-	else(users_choice="paper" and computers_choice="rock")
+	else if(users_choice=="paper" && computers_choice=="rock")
 	{
 		console.log("You won")
 	}
-	else(users_choice="paper" and computers_choice="scissors")
+	else if(users_choice=="paper" && computers_choice=="scissors")
 	{
 		console.log("Computer won")
 	}
-	else(users_choice="scissors" and computers_choice="rock")
+	else if(users_choice=="scissors" && computers_choice=="rock")
 	{
 		console.log("Computer won")
 	}
-	else(users_choice="scissors" and computers_choice="paper")
+	else if(users_choice=="scissors" && computers_choice=="paper")
 	{
 		console.log("You won")
 	}
