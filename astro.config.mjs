2cff6dccdab82dad8c51059ebac5b1237a7b2bf3
n0ncer <trajkodulic@gmail.com>
2026-01-22 18:41:57 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dusanio.github.io',
+  base: '/dusanio.github.io',
   integrations: [tailwind()],
 });
 
